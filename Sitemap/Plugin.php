66b9872df3f49104b1484ec83843132f1b9354f2
Ty <61855011+ty-yqs@users.noreply.github.com>
2020-12-31 22:15:51 +0800

    Create Plugin.php

---
+++ Sitemap/Plugin.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Sitemap 自动生成器<Br />由<a href="https://tyblog.com.cn" title="Ty">Ty</a>开发
+ * 
+ * @package XML Sitemap
+ * @author Ty
+ * @version 0.0.1
+ * @link https://tyblog.com.cn
+ */
+class Sitemap_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+		Helper::addRoute('sitemap', '/sitemap-maker.xml', 'Sitemap_Action', 'action');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate()
+	{
+		Helper::removeRoute('sitemap');
+	}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form){}
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+}
